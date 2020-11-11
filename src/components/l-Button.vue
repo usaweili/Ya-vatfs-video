16cc172531cdd212464667bb39f66fd6398c3eed
17374807741 <453007797@qq.com>
2020-11-11 18:07:43 +0800

    .

---
+++ src/components/l-Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button @click="$emit('click')"> 
+    <div class="loadingio-spinner-rolling-3tp4llk6hzy" v-if="loading">
+      <div class="ldio-yj7nunc9efi">
+        <div></div>
+      </div>
+    </div>
+    <slot v-else />
+  </button>
+
+</template>
+
+<script>
+export default {
+  name: 'l-Button',
+  props: {
+    loading: {
+      type: Boolean,
+      defalut: false
+    }
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
--- src/view/Main.vue
+++ src/view/Main.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="main">
-      <div class="search" :style="{ top : lists.length > 0 ? '10%' : '50%' }">
-          <input type="text" placeholder="请输入搜索关键字 !" v-model="form.name" />
-          <button @click="onSubmit">搜索</button>
+      <div class="search-wrap">
+        <div class="search">
+            <input type="text" placeholder="请输入搜索关键字 !" @keyup.enter="onSubmit" v-model="form.name" />
+            <l-botton @click="onSubmit" v-model:loading="loading" > 搜索 </l-botton>
+        </div>
       </div>
-      <div class="list-wrap">
+      <div class="list-wrap" v-if="lists.length > 0" :style="{'min-height': lists.length > 0 ? '100vh' : ''}" ref="list-wrap">
          <div class="list">
-            <div class="list-item" @click="getDetail(item)" :style="{'animation-delay': index * 50+'ms'}" v-for="(item,index) in lists" :key="index" >
+            <div class="list-item" @click="getDetail(item,index)" :style="{'animation-delay': index * 50+'ms'}" v-for="(item,index) in lists" :key="index" >
                 <img class="list-item-cover" :src="item.pic">
                 <div class="list-item-title">
                     {{ item.name }}
@@ -24,91 +26,103 @@
               {{ videoInfo.name }}
             </div>
             <div class="video-info-list">
-              <div class="video-info-list-item" @click="playerVideo(item)" v-for="(item,index) in videoInfo.playUrl" :key="index" >
+              <div class="video-info-list-item" :alt="item.title" :class="[item.active == true ? 'active' : '']" @click="playerVideo(item,index)" v-for="(item,index) in videoInfo.playUrl" :key="index" >
                 {{ item.title }}
               </div>
             </div>
         </div>
       </div>
-      <div class="video-wrap" id="dplayer"></div>
+      <div class="video-wrap" ref="dplayer"></div>
     </popup>
   </div>
 </template>
 
 <script>
 
-import Dplayer from 'Dplayer' 
+
+import videojs from "video.js"
+
+import DPlayer from "DPlayer"
 
 import api from "../common/http.api.js"
 
 import popup from "../components/Popup.vue"
+
+import lBotton from "../components/l-Button.vue"
+
 export default {
   name: 'Main',
   components:{
-      popup
+      popup,
+      lBotton
   },
   data() {
     return {
       count: 0,
       form: {
-          name : '斗罗大陆'
+          name : ''
       },
       type: false,
       lists: [],
       videoInfo: {},
-      src: null
+      loading: false
     }
-  },
-  mounted() {
-    this.$nextTick(() => {
-      // this.getVideo();
-    });
   },
   methods:{
       async onSubmit(){
-          const { data } = await api.getSearch({  kw: this.form.name  })
-          this.lists = data.data;
+          try {
+            this.loading =true;
+            const { data } = await api.getSearch({  kw: this.form.name  })
+            this.lists = data.data;
+            this.loading = false;
+            this.$nextTick(()=>{
+              window.scrollTo({
+                  top: document.querySelector('.list-wrap').offsetTop,
+                  behavior: "smooth",
+                })
+            })
+          } catch (error) {
+            this.loading = false;
+            window.alert('错误')
+            console.error(error);
+          }
+      
+          
       },
       async getDetail(item){
-          console.log(item);
          
           const { data } = await api.getDetail(item.vid);
           this.type = true;
-           this.poster = data.pic;
+          this.poster = data.pic;
           this.videoInfo = data;
           let arr = [];
           for (const iterator in this.videoInfo.playUrl) {
             arr.push({ title: iterator , src: this.videoInfo.playUrl[iterator]  })
           }
           this.videoInfo.playUrl = arr;
+           
       },
-      getVideo() {
-        this.myVideo = videojs("myVideo", {
-          bigPlayButton: true,
-          textTrackDisplay: false,
-          posterImage: false,
-          errorDisplay: false,
-          hls: {
-            withCredentials: true
-          }
-        });
-      },
-      playerVideo(item){
-
-        const dp = new Dplayer({
-            container: document.getElementById('dplayer'),
+      getVideo(src) {
+          this.dp = new DPlayer({
+            container: this.$refs.dplayer,
             video: {
-              type: 'hls',
-              url:  item.src
+                url: src,
+                type: 'hls',
+            },
+            pluginOptions: {
+                hls: {
+                    // hls config
+                },
             },
         });
-
-        dp.play()
-        console.log(dp);
-
+      },
+      playerVideo(item,index){
+        this.videoInfo.playUrl.forEach(listItem =>{
+            listItem.active = false;
+          })
+        this.videoInfo.playUrl[index].active = true;
+        this.getVideo(item.src);
        
-  
-        // console.log(this.myVideo);
       }
   }
 }
